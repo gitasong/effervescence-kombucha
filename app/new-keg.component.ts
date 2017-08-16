ca0188105c2c4c0bf7323e14cc22e8a0ec904077
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 15:08:36 -0700

    create New Keg form

--- app/new-keg.component.ts
+++ app/new-keg.component.ts
@@ -4,9 +4,21 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'new-task',
   template: `
+  <h2>Add New Keg</h2>
+  <div>
+    <label>Name: </label>
+    <input #newName>
+    <label>Name: </label>
+    <input #newBrand>
+    <label>Name: </label>
+    <input #newFlavor>
+    <label>Name: </label>
+    <input #newPrice>
+    <button>Add Keg</button>
+  </div>
   `
 })
 
 export class NewKegComponent {
-  
+
 }
