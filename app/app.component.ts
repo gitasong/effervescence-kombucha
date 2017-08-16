34a02eb4c328c35c4a4639919038ec03f6dbfb00
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 13:58:28 -0700

    fix typo in property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Keg } from './keg.model';
   template: `
   <div class="container">
     <h1>Effervesence Kombucha</h1>
-    <keg-list [childKegList] = "masterKegList"></keg-list>
+    <keg-list [childKegList]="masterKegList"></keg-list>
   </div>
   `
 })
