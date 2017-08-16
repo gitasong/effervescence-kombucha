5ba7f1ccfdc87147ea87030651d79af03e7b9f8c
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 15:10:08 -0700

    add NewKegComponent to root module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { KegListComponent } from './keg-list.component';
+import { NewKegComponent } from './new-keg.component';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent,
-                KegListComponent ],
+                KegListComponent,
+                NewKegComponent ],
   bootstrap:    [ AppComponent ]
 })
 
