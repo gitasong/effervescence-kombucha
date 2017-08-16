fa18541c7d2f84e6db0799c1399579e7cec54b29
Nicole Freed <nicole.freed@gmail.com>
2017-08-16 15:16:45 -0700

    import Forms module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { FormsModule }  from '@angular/forms';
 import { KegListComponent } from './keg-list.component';
 import { NewKegComponent } from './new-keg.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule,
+            FormsModule ],
   declarations: [ AppComponent,
                 KegListComponent,
                 NewKegComponent ],
