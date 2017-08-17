fb0e71066ba34e9bde9a73bb4e4150507f7b22ee
Nicole Freed <nicole.freed@gmail.com>
2017-08-17 10:06:34 -0700

    create edit-keg.component.ts

---
+++ app/edit-keg.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './keg.model';
+
+@Component({
+  selector: 'edit-task',
+  template: `
+  `
+
+export class EditKegComponent {
+  
+}
