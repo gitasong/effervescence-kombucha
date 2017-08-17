bd811114f82aac8046d681a2d4b65e6dbf117dff
Nicole Freed <nicole.freed@gmail.com>
2017-08-17 10:37:29 -0700

    create edit keg form; add @Input with selectedTask; change childSelectedTask to selectedTask

--- app/edit-keg.component.ts
+++ app/edit-keg.component.ts
@@ -1,11 +1,27 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Task } from './keg.model';
+import { Keg } from './keg.model';
 
 @Component({
   selector: 'edit-task',
   template: `
+  <div>
+      <div *ngIf="selectedKeg">
+        <h3>{{selectedKeg.name}}</h3>
+        <hr>
+        <h3>Edit Keg</h3>
+        <label>Enter New Keg Name:</label>
+        <input [(ngModel)]="selectedKeg.name">
+        <label>Enter New Keg Brand:</label>
+        <input [(ngModel)]="selectedKeg.brand">
+        <label>Enter New Keg Flavor:</label>
+        <input [(ngModel)]="selectedKeg.flavor">
+        <label>Enter New Keg Price:</label>
+        <input [(ngModel)]="selectedKeg.price">
+        <button>Done</button>
+      </div>
+    </div>
   `
 
 export class EditKegComponent {
-  
+  @Input() selectedTask: Task;
 }
